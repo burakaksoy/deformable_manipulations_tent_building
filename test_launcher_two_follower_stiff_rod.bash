e6d55f14433fb4305437c1ff0022b3d478accc7b
Burak Aksoy <burakaksoy20@gmail.com>
2024-03-30 21:59:39 -0400

    Update test_launcher_two_follower_stiff_rod.bash script for plotjuggler configs

--- test_launcher_two_follower_stiff_rod.bash
+++ test_launcher_two_follower_stiff_rod.bash
@@ -15,10 +15,13 @@ gnome-terminal --tab --title="GUI" --command "bash -c \"source ~/.bashrc; rosrun
 sleep 1s;
 
 # rqt_ez_publisher, to publish to "/space_nav/twist"
-gnome-terminal --tab --title="RQT_EZ_PUBLSHER" --command "bash -c \"source ~/.bashrc; sleep 2s; source ~/.bashrc; rosrun rosrun rqt_ez_publisher rqt_ez_publisher; exec bash\"";
+gnome-terminal --tab --title="RQT_EZ_PUBLSHER" --command "bash -c \"source ~/.bashrc; rosrun rqt_ez_publisher rqt_ez_publisher; exec bash\"";
 sleep 1s;
 
 gnome-terminal --tab --title="Controller" --command "bash -c \"source ~/.bashrc; roslaunch deformable_manipulations_tent_building velocity_controller.launch; exec bash\"";
+sleep 1s;
+
+gnome-terminal --title="PlotJuggler" --command  "bash -c \"source ~/.bashrc; rosrun plotjuggler plotjuggler --layout ~/catkin_ws_deformable/src/deformable_manipulations_tent_building/plotjuggler/dlo_sim_FT_limits_test_plotjuggler_real_time.xml; exec bash\"";
 sleep 1s;
 
 # gnome-terminal --tab --title="Controller" --command "bash -c \"source ~/.bashrc; roslaunch deformable_manipulations_tent_building velocity_controller_single_min_dist.launch; exec bash\"";
